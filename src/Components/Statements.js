9ff91734d54fdcce0b878786a33b06715abc08bd
Stephanie Raymos <sraymos@rlliquidators.com>
2021-02-16 13:51:44 -0800

    Statements Component

---
+++ src/Components/Statements.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Statements = () => {
+
+  document.title = "Return Center - Statements";
+
+  return (
+    <>
+      <h1>Statements</h1>
+    </>
+  );
+};
+
+export default Statements;
