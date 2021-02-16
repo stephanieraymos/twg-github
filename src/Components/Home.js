9dcf0642b182f3fd4c64d215747cb15a1065717f
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-02-16 09:09:03 -0800

    Home page

---
+++ src/Components/Home.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+
+const Home = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  document.title = "Return Center - Home";
+
+  return (
+    <>
+      <h1>Home</h1>
+    </>
+  );
+};
+
+export default Home;
