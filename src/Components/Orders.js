ed80e3abd0a21bc06d86f9f1cdb36595cc30938b
Stephanie Raymos <sraymos@rlliquidators.com>
2021-02-16 14:04:48 -0800

    Orders Component

---
+++ src/Components/Orders.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Orders = () => {
+
+  document.title = "Return Center - Orders";
+
+  return (
+    <>
+      <h1>Orders</h1>
+    </>
+  );
+};
+
+export default Orders;
