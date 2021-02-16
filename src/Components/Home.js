61caa3feee976c08490ec1dab65cc66f72ed0fbd
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-02-16 09:11:15 -0800

    Comments for understanding

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 
 const Home = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+// This will be the state of a modal that opens when button is clicked. 
+// The modal will show import information based on what info was requested.
 
   document.title = "Return Center - Home";
 
