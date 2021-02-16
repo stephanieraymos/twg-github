710b43249e1509d569d8c7c5aef08a2cbf7a98dc
Stephanie Raymos <sraymos@rlliquidators.com>
2021-02-16 11:27:10 -0800

    Working on baseplate for Modal

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,16 @@
 import React, { useState, useEffect } from "react";
+import Modal from "./Modal";
+
+const defaultState = {
+  isModalOpen: false,
+  modalContent: "",
+};
 
 const Home = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-// This will be the state of a modal that opens when button is clicked. 
-// The modal will show import information based on what info was requested.
+  // This will be the state of a modal that opens when button is clicked.
+  // The modal will show import information based on what info was requested.
+  const [modalContent, setModalContent] = useState("");
 
   document.title = "Return Center - Home";
 
