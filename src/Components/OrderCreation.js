3cd82d0a66094522696101958bd3149f9c9da240
Stephanie Raymos <sraymos@rlliquidators.com>
2021-02-17 12:58:55 -0800

    TP-30 Order Creation Component

---
+++ src/Components/OrderCreation.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const OrderCreation = () => {
+  document.title = "Order Creation";
+
+  return (
+    <>
+      <h1>Order Creation Page</h1>
+    </>
+  );
+};
+
+export default OrderCreation;
