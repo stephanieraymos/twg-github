96ff5624a1a5e63b3a83c93d052d2a13a5b9ebd9
Stephanie Raymos <sraymos@rlliquidators.com>
2021-02-17 12:08:18 -0800

    TP-26 Client Creation Component

---
+++ src/Components/ClientCreation.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ClientCreation = () => {
+  document.title = "Client Creation";
+
+  return (
+    <>
+      <h1>Client Creation</h1>
+    </>
+  );
+};
+
+export default ClientCreation;
