6b82795b5248d4b45ead5fc83e78d24c26fa13a3
Stephanie Raymos <sraymos@rlliquidators.com>
2021-02-17 10:41:02 -0800

    TP-39 Navigation Component

---
+++ src/Components/Navigation.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navigation = () => {
+  document.title = "Navigation";
+
+  return (
+    <>
+      <h1>Navigation</h1>
+    </>
+  );
+};
+
+export default Navigation;
