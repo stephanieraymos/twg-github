1e3dfc4aa77af5598403a9d96dff5cb280ab4653
Stephanie Raymos <sraymos@rlliquidators.com>
2021-02-18 11:55:42 -0800

    TP-53 Verification Component

---
+++ src/Components/Verification.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Verification = () => {
+  document.title = "Verify your identity";
+
+  return (
+    <>
+      <h1>Verify your identity</h1>
+    </>
+  );
+};
+
+export default Verification;
