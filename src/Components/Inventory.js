b92a8ae7191f1bf93be9d13a8a9ab7f5e61cffe0
Stephanie Raymos <sraymos@rlliquidators.com>
2021-02-18 11:48:18 -0800

    TP-51 Inventory Component

---
+++ src/Components/Inventory.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Inventory = () => {
+  document.title = "Inventory";
+
+  return (
+    <>
+      <h1>Inventory</h1>
+    </>
+  );
+};
+
+export default Inventory;
---
+++ src/Components/OrderDetails.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const OrderDetails = () => {
+  document.title = "Order Details";
+
+  return (
+    <>
+      <h1>Order Details</h1>
+    </>
+  );
+};
+
+export default OrderDetails;
