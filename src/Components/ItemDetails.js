7ef8e6d4c8f9da32ae6e2a2cab0a80ffcd252f8a
Stephanie Raymos <sraymos@rlliquidators.com>
2021-02-18 11:53:31 -0800

    TP-52 Item Details Component

---
+++ src/Components/ItemDetails.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ItemDetails = () => {
+  document.title = "Item Details";
+
+  return (
+    <>
+      <h1>Item Details</h1>
+    </>
+  );
+};
+
+export default ItemDetails;
