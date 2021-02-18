35a6bea3534560b217d22e99a59d9fbe3a34ee96
Stephanie Raymos <sraymos@rlliquidators.com>
2021-02-17 16:55:40 -0800

    TP-49 Order Details Component

---
+++ src/Components/OrderDetail.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const OrderDetail = () => {
+  document.title = "Order Details";
+
+  return (
+    <>
+      <h1>Order Details</h1>
+    </>
+  );
+};
+
+export default OrderDetail;
