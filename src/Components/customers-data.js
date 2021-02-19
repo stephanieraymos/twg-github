0d6776fab7c12739329357ad5e43a714ab02c0b6
Stephanie Raymos <sraymos@rlliquidators.com>
2021-02-19 10:49:17 -0800

    TP-58 Customer mock data

---
+++ src/Components/customers-data.js
@@ -0,0 +1,28 @@
+const customers = [
+  {
+    id: 1,
+    name: "Wayfair",
+    orders: 54,
+  },
+  {
+    id: 2,
+    name: "Sally Shoemaker",
+    orders: 5,
+  },
+  {
+    id: 3,
+    name: "Ivan Ivenhouser",
+    orders: 68,
+  },
+  {
+    id: 4,
+    name: "Matt Matthewson",
+    orders: 36,
+  },
+  {
+    id: 5,
+    name: "David Davidson",
+    orders: 28,
+  },
+];
+export default customers;
