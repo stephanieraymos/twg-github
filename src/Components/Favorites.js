b6efadfc40c5e609e8410775557f92e3aa35941e
Stephanie Raymos <sraymos@rlliquidators.com>
2021-02-19 14:49:14 -0800

    TP-54 Favorites Component

---
+++ src/Components/Favorites.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Favorites = () => {
+  document.title = "Favorites";
+
+  return (
+    <>
+      <h1>Favorites</h1>
+    </>
+  );
+};
+
+export default Favorites;
+
+//TP-54
