679f49a9528a1d13b20d2033bbb17e0986dc6757
Stephanie Raymos <sraymos@rlliquidators.com>
2021-02-19 10:25:26 -0800

    TP-39 Navigation: Modal

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,18 +1,11 @@
 import React, { useState } from "react";
-import Modal from './Modal';
 
 const Home = () => {
-
   document.title = "Home";
-
-  const showModal = (e) => {
-      return <Modal />
-  }
 
   return (
     <>
       <h1>Home Page</h1>
-      <button type="button" onClick={(e) => showModal()}></button>
     </>
   );
 };
--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,11 +1,17 @@
 import React from "react";
+import Modal from "./Modal";
 
 const Navigation = () => {
   document.title = "Navigation";
 
+  const showModal = (e) => {
+    return <Modal />;
+  };
+
   return (
     <>
       <h1>Navigation</h1>
+      <button type="button" onClick={(e) => showModal()}></button>
     </>
   );
 };
