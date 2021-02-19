59b4827905a5c6163b548b4decac57dad3c05c69
Stephanie Raymos <sraymos@rlliquidators.com>
2021-02-18 16:16:45 -0800

    Client Details + removal of unneeded components

---
+++ src/Components/ClientDetails.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ClientDetails = () => {
+  document.title = "Client Details";
+
+  return (
+    <>
+      <h1>Client Details Page</h1>
+    </>
+  );
+};
+
+export default ClientDetails;
