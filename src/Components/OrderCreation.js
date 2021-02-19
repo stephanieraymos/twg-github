caffa97de95e371e9f96f1efc186b2eb5f75529f
Stephanie Raymos <sraymos@rlliquidators.com>
2021-02-19 08:46:26 -0800

    Order Creation component

---
+++ src/Components/OrderCreation.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const OrderCreation = () => {
+  document.title = "Order Creation";
+
+  return (
+    <>
+      <h1>Order Creation</h1>
+    </>
+  );
+};
+
+export default OrderCreation;
