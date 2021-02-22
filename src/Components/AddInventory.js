e80f281c62763fbc3ff01b529a76fa238db88ef8
Stephanie Raymos <sraymos@rlliquidators.com>
2021-02-22 09:28:32 -0800

    Add inventory component

---
+++ src/Components/AddInventory.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AddInventory = () => {
+  document.title = "Add Inventory";
+
+  return (
+    <>
+      <h1>Add Inventory</h1>
+    </>
+  );
+};
+
+export default AddInventory;
