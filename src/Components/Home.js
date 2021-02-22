4657c15321d6d40a835c0235026bb3aebb9fc445
Stephanie Raymos <sraymos@rlliquidators.com>
2021-02-22 12:04:41 -0800

    Video showing only if screen is large enough

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import video from "../img/video.mp4";
+import warehouse from "../img/warehouse.jpg";
 
 const Home = () => {
   document.title = "Home";
@@ -7,9 +8,9 @@ const Home = () => {
   return (
     <>
       <div className="container">
-        <video className="bg-video" autoPlay loop muted>
-          <source src={video} type="video/mp4" />
-        </video>
+        {window.innerWidth >= 800 ? <video className="bg-video" autoPlay loop muted>
+          <source src={video} type="video/mp4"/>
+        </video> : <img src={warehouse} alt="Warehouse image"/>}
         <div className="content">
           <div className="sub-content">
             <h1 className="home-header">The WholeSale Group</h1>
