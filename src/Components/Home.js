fcbec6eab601ab7ac41458624e0c92882a5b8f27
Stephanie Raymos <sraymos@rlliquidators.com>
2021-02-22 09:17:39 -0800

    Video background done

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,13 +1,23 @@
 import React, { useState } from "react";
+import video from "../img/video.mp4";
 
 const Home = () => {
   document.title = "Home";
 
   return (
     <>
-      <h1 className="home-header">The WholeSale Group</h1>
-      <div className="btn-container">
-        <button className="btn">See more</button>
+      <div className="container">
+        <video className="bg-video" autoPlay loop muted>
+          <source src={video} type="video/mp4" />
+        </video>
+        <div className="content">
+          <div className="sub-content">
+            <h1 className="home-header">The WholeSale Group</h1>
+            <div className="btn-container">
+              <button className="btn">See more</button>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
