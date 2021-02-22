28349840a3265cece31f60e20bb2eccd1c4826a2
Stephanie Raymos <sraymos@rlliquidators.com>
2021-02-22 14:49:00 -0800

    TP-43 About and Contact set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Contact from './Components/Contact'
 import Home from './Components/Home';
 
 function App() {
   return (
     <div>
-      <Home />
+      <Contact />
     </div>
   )
 }
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -5,8 +5,19 @@ const Contact = () => {
 
   return (
     <>
-      <main className="heading">About us</main>
-      <div className="underline"></div>
+      <div className="container">
+        <h1 className="heading">About us</h1>
+        <div className="underline"></div>
+        <p className="body-text">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur, ut
+          corporis. Voluptates quas, magni quisquam possimus id quam cupiditate
+          odio distinctio similique, atque fugit consequuntur aperiam esse nihil
+          voluptatum neque. Lorem, ipsum dolor sit amet consectetur adipisicing
+          elit. Pariatur, ut corporis. Voluptates quas, magni quisquam possimus
+          id quam cupiditate odio distinctio similique, atque fugit consequuntur
+          aperiam esse nihil voluptatum neque.
+        </p>
+      </div>
     </>
   );
 };
