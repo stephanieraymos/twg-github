46b7661ce062a397e8179f0482049e00aa1efdb8
Stephanie Raymos <sraymos@rlliquidators.com>
2021-02-22 16:49:48 -0800

    Grid layout edits

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -6,45 +6,34 @@ const Contact = () => {
   return (
     <>
       <div className="container">
-        <h1 className="heading">About us</h1>
-        <div className="underline"></div>
-        <p className="body-text">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur, ut
-          corporis. Voluptates quas, magni quisquam possimus id quam cupiditate
-          odio distinctio similique, atque fugit consequuntur aperiam esse nihil
-          voluptatum neque. Lorem, ipsum dolor sit amet consectetur adipisicing
-          elit. Pariatur, ut corporis. Voluptates quas, magni quisquam possimus
-          id quam cupiditate odio distinctio similique, atque fugit consequuntur
-          aperiam esse nihil voluptatum neque.
-        </p>
-      </div>
-      <div className="container">
-        <h1 className="heading">Contact</h1>
-        <div className="underline"></div>
         <div className="grid-container">
-          <p className="grid-items grid1">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur,
-            ut corporis. Voluptates quas, magni quisquam possimus id quam
-            cupiditate odio distinctio similique, atque fugit consequuntur
-            aperiam esse nihil voluptatum neque. Lorem, ipsum dolor sit amet
-            consectetur adipisicing elit. Pariatur, ut corporis. Voluptates
-            quas, magni quisquam possimus id quam cupiditate odio distinctio
-            similique, atque fugit consequuntur aperiam esse nihil voluptatum
-            neque.
-          </p>
-          <p className="grid-items grid2">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur,
-            ut corporis. Voluptates quas, magni quisquam possimus id quam
-            cupiditate odio distinctio similique, atque fugit consequuntur
-            aperiam esse nihil voluptatum neque. Lorem, ipsum dolor sit amet
-            consectetur adipisicing elit. Pariatur, ut corporis. Voluptates
-            quas, magni quisquam possimus id quam cupiditate odio distinctio
-            similique, atque fugit consequuntur aperiam esse nihil voluptatum
-            neque.
-          </p>
-          <p className="grid-items grid3">
-            email@email.com
-          </p>
+          <div className="grid1 grid-items">
+            <h1 className="heading">About us</h1>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Pariatur, ut corporis. Voluptates quas, magni quisquam possimus id
+              quam cupiditate odio distinctio similique, atque fugit
+              consequuntur aperiam esse nihil voluptatum neque. Lorem, ipsum
+              dolor sit amet consectetur adipisicing elit. Pariatur, ut
+              corporis. Voluptates quas, magni quisquam possimus id quam
+              cupiditate odio distinctio similique, atque fugit consequuntur
+              aperiam esse nihil voluptatum neque.
+            </p>
+          </div>
+          <div className="grid2 grid-items">
+            <h1 className="heading">Contact us</h1>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Pariatur, ut corporis. Voluptates quas, magni quisquam possimus id
+              quam cupiditate odio distinctio similique, atque fugit
+              consequuntur aperiam esse nihil voluptatum neque. Lorem, ipsum
+              dolor sit amet consectetur adipisicing elit. Pariatur, ut
+              corporis. Voluptates quas, magni quisquam possimus id quam
+              cupiditate odio distinctio similique, atque fugit consequuntur
+              aperiam esse nihil voluptatum neque.
+            </p>
+          </div>
+          <p className="grid-items grid3">email@email.com</p>
         </div>
       </div>
     </>
