90d9907b46c4a5ee3a39636d0d6ab149aececead
Stephanie Raymos <sraymos@rlliquidators.com>
2021-02-23 14:42:22 -0800

    TP-43 Started over on grid styling

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,55 +1,41 @@
 import React from "react";
+import grid from "../grid.css";
 
 const Contact = () => {
   document.title = "Contact";
 
   return (
     <>
-        <div className="grid-container">
-          <div className="menu-grid grid-items-menu">
-              <p>HOME</p>
-              <p>ORDERS</p>
-              <p>INVENTORY</p>
-              <p>STATEMENTS</p>
-          </div>
-          <div className="about-grid grid-items">
-            <h1 className="heading">About</h1>
-            <div className="underline"></div>
-          </div>
-          <div className="info-grid grid-items">
-            <h1 className="heading">Info</h1>
-            <div className="underline"></div>
-          </div>
-          <div className="paragraph1 grid-items">
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Pariatur, ut corporis. Voluptates quas, magni quisquam possimus id
-              quam cupiditate odio distinctio similique, atque fugit
-              consequuntur aperiam esse nihil voluptatum neque. Lorem, ipsum
-              dolor sit amet consectetur adipisicing elit. Pariatur, ut
-              corporis. Voluptates quas, magni quisquam possimus id quam
-              cupiditate odio distinctio similique, atque fugit consequuntur
-              aperiam esse nihil voluptatum neque.
-            </p>
-          </div>
-          <div className="paragraph2 grid-items">
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Pariatur, ut corporis. Voluptates quas, magni quisquam possimus id
-              quam cupiditate odio distinctio similique, atque fugit
-              consequuntur aperiam esse nihil voluptatum neque. Lorem, ipsum
-              dolor sit amet consectetur adipisicing elit. Pariatur, ut
-              corporis. Voluptates quas, magni quisquam possimus id quam
-              cupiditate odio distinctio similique, atque fugit consequuntur
-              aperiam esse nihil voluptatum neque.
-            </p>
-          </div>
-          <div className="email-grid grid-items">
-            <p>Sales: email@email.com</p>
-            <p>Support: email@email.com</p>
-            <p>Upline: email@email.com</p>
-          </div>
+      <div className="grid-container">
+        <div className="header-grid">The WholeSale Group</div>
+        <div className="menu-grid">
+          <ul>
+            <li>HOME</li>
+            <li>ORDERS</li>
+            <li>INVENTORY</li>
+            <li>STATEMENTS</li>
+          </ul>
         </div>
+        <div className="about-grid">
+          <div className="section-header">About</div>
+          <div className="underline"></div>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur,
+            ut corporis. Voluptates quas, magni quisquam possimus id quam
+            cupiditate odio distinctio similique, atque fugit consequuntur
+            aperiam esse nihil voluptatum neque. Lorem, ipsum dolor sit amet
+            consectetur adipisicing elit. Pariatur, ut corporis. Voluptates
+            quas, magni quisquam possimus id quam cupiditate odio distinctio
+            similique, atque fugit consequuntur aperiam esse nihil voluptatum
+            neque.
+          </p>
+        </div>
+        <div className="email-grid">
+          <p>Sales: email@email.com</p>
+          <p>Support: email@email.com</p>
+          <p>Upline: email@email.com</p>
+        </div>
+      </div>
     </>
   );
 };
