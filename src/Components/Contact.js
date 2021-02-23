6169a49dd2198318c18bed466abbaa4ffa11de5e
Stephanie Raymos <sraymos@rlliquidators.com>
2021-02-23 08:59:18 -0800

    TP-43 Working on grid

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -9,19 +9,11 @@ const Contact = () => {
         <div className="grid-container">
           <div className="grid1 grid-items">
             <h1 className="heading">About us</h1>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Pariatur, ut corporis. Voluptates quas, magni quisquam possimus id
-              quam cupiditate odio distinctio similique, atque fugit
-              consequuntur aperiam esse nihil voluptatum neque. Lorem, ipsum
-              dolor sit amet consectetur adipisicing elit. Pariatur, ut
-              corporis. Voluptates quas, magni quisquam possimus id quam
-              cupiditate odio distinctio similique, atque fugit consequuntur
-              aperiam esse nihil voluptatum neque.
-            </p>
           </div>
           <div className="grid2 grid-items">
             <h1 className="heading">Contact us</h1>
+          </div>
+          <div className="grid3 grid-items">
             <p>
               Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               Pariatur, ut corporis. Voluptates quas, magni quisquam possimus id
@@ -33,7 +25,23 @@ const Contact = () => {
               aperiam esse nihil voluptatum neque.
             </p>
           </div>
-          <p className="grid-items grid3">email@email.com</p>
+          <div className="grid4 grid-items">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Pariatur, ut corporis. Voluptates quas, magni quisquam possimus id
+              quam cupiditate odio distinctio similique, atque fugit
+              consequuntur aperiam esse nihil voluptatum neque. Lorem, ipsum
+              dolor sit amet consectetur adipisicing elit. Pariatur, ut
+              corporis. Voluptates quas, magni quisquam possimus id quam
+              cupiditate odio distinctio similique, atque fugit consequuntur
+              aperiam esse nihil voluptatum neque.
+            </p>
+          </div>
+          <div className="grid5 grid-items">
+            <p>Sales: email@email.com</p>
+            <p>Support: email@email.com</p>
+            <p>Upline: email@email.com</p>
+          </div>
         </div>
       </div>
     </>
