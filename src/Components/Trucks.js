c7a6d940819e84ecda8c6efaa785324b4bc9567a
Stephanie Raymos <sraymos@rlliquidators.com>
2021-02-24 10:37:27 -0800

    Trucks Component

---
+++ src/Components/Trucks.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Trucks = () => {
+  document.title = "Truck List";
+
+  return (
+    <>
+      <h1>List of Trucks</h1>
+    </>
+  );
+};
+
+export default Trucks;
