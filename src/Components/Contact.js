6794989eed569b9bf2cc5b0a66236dbacb6f1235
Stephanie Raymos <sraymos@rlliquidators.com>
2021-02-24 09:38:02 -0800

    Working on grid

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -25,7 +25,37 @@ const Contact = () => {
             consectetur adipisicing elit. Pariatur, ut corporis. Voluptates
             quas, magni quisquam possimus id quam cupiditate odio distinctio
             similique, atque fugit consequuntur aperiam esse nihil voluptatum
-            neque.
+            neque. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Enim consectetur pariatur, natus nulla deleniti sapiente cupiditate
+            ipsam officia rerum sunt fugit est dolor totam animi repellendus?
+            Quod voluptate unde vitae tenetur delectus, repudiandae voluptas,
+            dolorum quaerat vel dolor voluptatem dolores saepe suscipit officia
+            quisquam. Porro repudiandae nesciunt distinctio itaque perspiciatis.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+            excepturi repellendus vitae sunt itaque, quis vel non deleniti
+            officiis ullam, ipsam enim qui aperiam placeat laborum nam. Tempora
+            cum saepe tenetur ea, fugiat rem eaque et corrupti praesentium,
+            dolores adipisci veniam sit. Ex quidem nam culpa, reiciendis
+            possimus ipsam nostrum assumenda repudiandae quae, unde deleniti
+            quos. Adipisci amet voluptatibus debitis nesciunt a earum iure eos
+            dignissimos asperiores. Nobis ipsam culpa quam molestiae fuga
+            expedita quod deserunt corrupti, eveniet nam numquam laborum non
+            minus minima, dicta unde saepe quidem?
+          </p>
+          <p>
+            Numquam error, fugiat fugit atque incidunt nam aperiam quae autem
+            exercitationem blanditiis voluptatem facere sunt labore omnis,
+            molestiae sed corrupti temporibus, laborum quo. Quisquam dolor nihil
+            eligendi id eos natus perferendis doloribus fuga sed. Tempore eum
+            quam reiciendis, beatae, deserunt consequuntur laboriosam omnis,
+            iusto ipsa libero dolore recusandae tenetur inventore autem ducimus
+            rem. Ipsam commodi molestiae asperiores iste laboriosam minus
+            corrupti quo neque error nulla voluptate adipisci similique est,
+            obcaecati cum quidem dolore, aliquid vitae? Laboriosam, nulla
+            officiis, numquam delectus nisi a quaerat porro consequatur rem
+            omnis hic. Laboriosam quam maiores nemo, veniam voluptas eaque
+            temporibus itaque quo minus officiis animi officia placeat sint
+            sequi cumque vitae doloremque nisi, dolores eligendi? Possimus?
           </p>
         </div>
         <div className="email-grid all-grid-items">
