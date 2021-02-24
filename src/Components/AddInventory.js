c0c1df71dd6ff4b4dc1d487643530a7674cdce0a
Stephanie Raymos <sraymos@rlliquidators.com>
2021-02-24 15:53:42 -0800

    Listing truck load items

--- src/Components/AddInventory.js
+++ src/Components/AddInventory.js
@@ -5,21 +5,21 @@ import Inventory from "./Inventory";
 const AddInventory = () => {
   document.title = "Add Inventory";
   const [truckLoad, setTruckLoad] = useState([]);
-  const [truckName, setTruckName] = useState('');
-  const [truckPrice, setTruckPrice] = useState('');
+  const [truckName, setTruckName] = useState("");
+  const [truckPrice, setTruckPrice] = useState("");
   const [truckContents, setTruckContents] = useState([]);
   const [error, setError] = useState(false);
   const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
 
-  const contents = truckContents.join(","); //All values joined + seperated by commas
+  // const contents = truckContents.join(","); //All values joined + seperated by commas
 
   const handleSubmit = (e) => {
     e.preventDefault();
     try {
-      let newTruckLoad = [{truckName, truckPrice, truckContents}];
-      setTruckLoad(newTruckLoad)
+      let newTruckLoad = [{ truckName, truckPrice, truckContents }];
+      setTruckLoad(newTruckLoad);
       console.log(newTruckLoad);
-      return <p>{newTruckLoad}</p>
+      return <p>{newTruckLoad}</p>;
     } catch (error) {
       console.log(error);
       setError(true);
@@ -28,46 +28,45 @@ const AddInventory = () => {
 
   return (
     <>
-      <section className="container-main">
+      <section className="section-center">
         <h3 className="heading">Add Truckload</h3>
         <form onSubmit={handleSubmit}>
-        {alert.show && <Alert />}
-          <input
-            className="input"
-            type="text"
-            value={truckName}
-            onChange={(e) => setTruckName(e.target.value)}
-            placeholder="Name of Truck"
-            style={{textAlign: "center"}}
-          />
-          <input
-            className="input"
-            type="number"
-            value={truckPrice}
-            onChange={(e) => setTruckPrice(e.target.value)}
-            placeholder="Price"
-            style={{textAlign: "center"}}
-          />
-          <input
-            className="input"
-            type="text"
-            value={truckContents}
-            onChange={(e) => setTruckContents(e.target.value)}
-            placeholder="What's in the truck?"
-            style={{textAlign: "center"}}
-          />
-          <button className="btn btn-center" type="submit">
-            Submit
-          </button>
+          {alert.show && <Alert />}
+          <div className="form-control">
+            <input
+              type="text"
+              value={truckName}
+              onChange={(e) => setTruckName(e.target.value)}
+              placeholder="Name of Truck"
+              style={{ textAlign: "center" }}
+            />
+            <input
+              type="number"
+              value={truckPrice}
+              onChange={(e) => setTruckPrice(e.target.value)}
+              placeholder="Price"
+              style={{ textAlign: "center" }}
+            />
+            <input
+              type="text"
+              value={truckContents}
+              onChange={(e) => setTruckContents(e.target.value)}
+              placeholder="What's in the truck?"
+              style={{ textAlign: "center" }}
+            />
+            <button className="btn btn-center" type="submit">
+              Submit
+            </button>
+          </div>
         </form>
-        <Inventory />
+        <Inventory truckLoad={truckLoad} />
       </section>
-      <section className="section">
+      {/* <section className="section">
         <h1>Truck: {truckName}</h1>
         <div className="underline"></div>
         <p>Price: ${truckPrice}</p>
         <p>Contents: {contents}</p>
-      </section>
+      </section> */}
     </>
   );
 };
--- src/Components/Inventory.js
+++ src/Components/Inventory.js
@@ -1,14 +1,31 @@
 import React from "react";
+import { FaEdit, FaTrash } from "react-icons/fa";
 
-const Inventory = () => {
-  document.title = "Inventory";
-
+const Inventory = ({ truckLoad }) => {
   return (
-    <>
-      <h1>Inventory List</h1>
-    </>
+    <div className="truckLoad-list">
+      {truckLoad.map((item) => {
+        const { id, truckName, truckPrice, truckContents } = item;
+        return (
+          <article key={id} className="truckLoad">
+            <p className="items">Truck name: {truckName}</p>
+            <p className="items">Price: {truckPrice}</p>
+            <p className="items">Contents: {truckContents}</p>
+            <div className="btn-container">
+              <button type="button" className="edit-btn">
+                <FaEdit />
+              </button>
+              <button type="button" className="delete-btn">
+                <FaTrash />
+              </button>
+            </div>
+          </article>
+        );
+      })}
+    </div>
   );
 };
+
 
 export default Inventory;
 
