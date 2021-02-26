2b88e39e504e62055f3372790a372052313d89e6
Stephanie Raymos <sraymos@rlliquidators.com>
2021-02-26 15:06:42 -0800

    TP-39 Social icons

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -33,6 +33,16 @@ const Navigation = () => {
             </ul>
           </div>
         )}
+        <ul className="social-icons">
+          {social.map((socialIcon) => {
+            const { id, url, icon } = socialIcon;
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </nav>
   );
