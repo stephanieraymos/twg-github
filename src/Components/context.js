64a82e0771aac5b8bd0613e81c2a908a89852ed2
Stephanie Raymos <sraymos@rlliquidators.com>
2021-03-02 11:38:56 -0800

    Created state values + functions for sidebar and modal + added the values to AppContext

--- src/Components/context.js
+++ src/Components/context.js
@@ -4,7 +4,39 @@ const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
   //Wrapping whole app in Provider
-  return <AppContext.Provider value="Testing">{children}</AppContext.Provider>;
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isModalOpen,
+        isSidebarOpen,
+        openModal,
+        openSidebar,
+        closeModal,
+        closeSidebar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 // Custom hook
