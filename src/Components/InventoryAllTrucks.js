82b77a6b505cd0d1b18ce4a225cfe632e5c194d1
Stephanie Raymos <sraymos@rlliquidators.com>
2021-03-03 13:25:22 -0800

    Table centered

--- src/Components/InventoryAllTrucks.js
+++ src/Components/InventoryAllTrucks.js
@@ -14,7 +14,7 @@ function Inventory() {
     const newTrucks = await response.json(); //returns a promise
     setTrucks(newTrucks); //Making sure the trucks list is current using newTrucks which adds each new truck to the truckLoad
   };
-  
+
   //useEffect fetches trucks only after initial render. This is accomplished by passing the empty array
   useEffect(() => {
     fetchTrucks();
@@ -23,25 +23,27 @@ function Inventory() {
 
   return (
     <>
-      <div className="header-items">
-        <p className="truck">TRUCK NAME</p>
-        <p className="price">PRICE</p>
-        <p className="contents">CONTENTS</p>
-        <p className="side-btn-container"></p>
-      </div>
+      <div className="table-wrapper">
+        <div className="header-items">
+          <p className="truck">TRUCK NAME</p>
+          <p className="price">PRICE</p>
+          <p className="contents">CONTENTS</p>
+          <p className="side-btn-container"></p>
+        </div>
 
-      <div className="truckLoad-list">
-        {trucks.map((truck) => {
-          const { id, truckName, truckPrice, truckContents } = truck;
+        <div className="truckLoad-list">
+          {trucks.map((truck) => {
+            const { id, truckName, truckPrice, truckContents } = truck;
 
-          return (
-            <div className="truckLoad" key={truck.id}>
-              <p className="items truck">{truck.truckName}</p>
-              <p className="items price">${truck.truckPrice}</p>
-              <p className="items contents">{truck.truckContents}</p>
-            </div>
-          );
-        })}
+            return (
+              <div className="truckLoad" key={truck.id}>
+                <p className="items truck">{truck.truckName}</p>
+                <p className="items price">${truck.truckPrice}</p>
+                <p className="items contents">{truck.truckContents}</p>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </>
   );
