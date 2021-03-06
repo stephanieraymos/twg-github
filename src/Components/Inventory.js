bf9360751d87e0c73cb78e6aca1b92d638b7ec65
Stephanie Raymos <sraymos@rlliquidators.com>
2021-03-05 16:35:07 -0800

    Added place for manifest in inventory

--- src/Components/Inventory.js
+++ src/Components/Inventory.js
@@ -9,18 +9,20 @@ const Inventory = ({ truckLoad, removeItem, editItem }) => {
         <p className="truck">TRUCK NAME</p>
         <p className="price">PRICE</p>
         <p className="contents">CONTENTS</p>
+        <p className="manifest">MANIFEST</p>
         <p className="side-btn-container"></p>
       </div>
       
       <div className="truckLoad-list">
         {truckLoad.map((truck) => {
-          const { id, truckName, truckPrice, truckContents } = truck;
+          const { id, truckName, truckPrice, truckContents, truckManifest } = truck;
           return (
             <>
               <article key={id} className="truckLoad">
                 <p className="items truck">{truckName}</p>
                 <p className="items price">${truckPrice}</p>
                 <p className="items contents">{truckContents}</p>
+                <p className="items manifest">{truckManifest}</p>
                 <div className="side-btn-container">
                   <button
                     type="button"
