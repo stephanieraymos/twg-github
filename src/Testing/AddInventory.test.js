d580099ed1d191368dd8162ee29353fb806238d0
Stephanie Raymos <sraymos@rlliquidators.com>
2021-03-10 15:35:36 -0800

    Testing AddInventory

---
+++ src/Testing/AddInventory.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import AddInventory from "../Components/AddInventory"
+
+describe("AddTrucks", () => {
+  it("Adds the truck to inventory", () => {
+    const { getByText } = render(<AddInventory text="Hi" />);
+
+    expect(getByText("Hi")).toBeTruthy();
+  });
+});
