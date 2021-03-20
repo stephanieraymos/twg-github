a16dd7ac235ba8287082c37da80ae5981c37ca01
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-03-19 20:57:26 -0700

    Fixed merge conflicts

--- src/Components/InventoryAllTrucks.js
+++ src/Components/InventoryAllTrucks.js
@@ -13,11 +13,6 @@ const InventoryAllTrucks = () => {
   const [truckFile, setTruckFile] = useState([]);
 
   const openModal = () => {
-<<<<<<< HEAD
-    // e.preventDefault()
-    //getManifest(truckManifest);
-=======
->>>>>>> stephanie
     setIsModalOpen(true);
   };
 
@@ -28,39 +23,6 @@ const InventoryAllTrucks = () => {
   const { trucks } = useTruckContext();
 
   //^ GET MANIFEST REQUEST //
-<<<<<<< HEAD
-  // useEffect(() => {
-    const getManifest = (truckManifestId) => {
-      try {
-        const data = new FormData();
-        truckManifestId.map(id => data.append("truckManifestId", id))
-        fetch(
-          "http://143.110.225.28/api/v1/inventory/manifest/",
-          {
-            method: "POST",
-            body: data,
-          }
-        )
-        .then(response => response.json())
-        .then(manifest => setTruckFile(manifest))
-        .then(_ => openModal());
-
-        // console.log("log inside getManifest: (truckManifest)", truckManifestId);
-        // console.log("log inside getManifest: (response)", response);
-        // const json = await response.json();
-        // // console.log("log inside getManifest: (data)", data);
-        // console.log(json);
-        // return json;
-        // const file = await json["truckManifest"];
-        // console.log(file);
-        // window.location.assign([file]);
-        // console.log([truckManifestName])
-      } catch (error) {
-        console.log(error);
-      }
-    };
-  // }, []);
-=======
   const getManifest = (truckManifestId) => {
     try {
       const data = new FormData();
@@ -76,7 +38,6 @@ const InventoryAllTrucks = () => {
       console.log(error);
     }
   };
->>>>>>> stephanie
 
   return (
     <>
@@ -106,15 +67,6 @@ const InventoryAllTrucks = () => {
 
             return (
               <div className="truckLoad" key={id}>
-<<<<<<< HEAD
-                <p className="items all-trucks-name">{truckName}</p>
-                <p className="items all-trucks-price">${truckPrice}</p>
-                <p className="items all-trucks-contents">{truckContents}</p>
-                <button onClick={() => {
-                  getManifest(truckManifestId)
-                }}>
-                  {/* <button onClick={openModal}> */}
-=======
                 <p className="items all-trucks-name text-truncate">
                   {truckName}
                 </p>
@@ -130,42 +82,15 @@ const InventoryAllTrucks = () => {
                     getManifest(truckManifestId);
                   }}
                 >
->>>>>>> stephanie
                   <p className="items all-trucks-manifest">
                     <img src={folder} alt="download icon" />
                   </p>
                 </button>
-
-<<<<<<< HEAD
-                <Modal show={isModalOpen} onHide={closeModal}>
-<<<<<<< HEAD
-                  <Modal.Header style={{ color: "white" }}>
-                    Manifests for this truck
-                  </Modal.Header>
-                  <Modal.Body>
-                    {/*//^ Map method to get list of files for each truck inside modal */}
-                    {truckFile.map((manifest) => {
-                      const { truckManifest, truckManifestName } = manifest;
-                      return (
-                        <ul>
-                          <li
-                            key={truckManifestName}
-                            onClick={() => window.location.assign(truckManifest)}
-                          >
-                            {truckManifestName}
-                          </li>
-                        </ul>
-                      );
-                    })}
-                  </Modal.Body>
-=======
-=======
                 <Modal
                   show={isModalOpen}
                   onHide={closeModal}
                   dialogClassName="files-modal"
                 >
->>>>>>> stephanie
                   <Modal.Dialog
                     style={{
                       textAlign: "center",
@@ -209,9 +134,7 @@ const InventoryAllTrucks = () => {
                       })}
                     </Modal.Body>
                   </Modal.Dialog>
->>>>>>> stephanie
                 </Modal>
-
               </div>
             );
           })}
