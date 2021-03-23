d2c040eec57a9fd6ffc6622d53a8b88608704e0f
Stephanie Raymos <sraymos@rlliquidators.com>
2021-03-23 09:44:05 -0700

    Merging Iev's addition of company, phone number + billing address

--- src/context.js
+++ src/context.js
@@ -1,55 +1,41 @@
 import React, { useState, useEffect, useContext, useReducer } from "react";
-
-
 // Generating context
 const AppContext = React.createContext(null);
-
 //Generating provider
 const AppProvider = ({ children }) => {
   const url = "http://143.110.225.28/api/v1/inventory/"; //API LINK
-
   //////////////////////// &&--STATE--&& /////////////////////////////
-
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
   const [editId, setEditId] = useState(null);
   const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
   const [error, setError] = useState(false);
-
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
   const [userId, setUserId] = useState("d73897ef-9b70-463f-8dc1-bdafbe8891ff");
-
-
+  const [company, setCompany] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [billingAddress, setBillingAddress] = useState("");
   ////////////////////// &&--FUNCTIONS--&& /////////////////////////
-
   const openSidebar = () => {
     setIsSidebarOpen(true);
   };
-
   const closeSidebar = () => {
     setIsSidebarOpen(false);
   };
-
   const openModal = () => {
     setIsModalOpen(true);
   };
-
   const closeModal = () => {
     setIsModalOpen(false);
   };
-
-  
 
   ////////////////////////// &&--PROVIDER--&& ///////////////////////////////
   return (
     <AppContext.Provider
       value={{
-
         isEditing,
         setIsEditing,
         editId,
@@ -62,34 +48,32 @@ const AppProvider = ({ children }) => {
         openSidebar,
         closeModal,
         closeSidebar,
-
         isSidebarOpen,
         isModalOpen,
         setIsSidebarOpen,
         setIsModalOpen,
-
         firstName,
         setFirstName,
         lastName,
         setLastName,
         email,
         setEmail,
-        password,
-        setPassword,
-        confirmPassword,
-        setConfirmPassword,
         userId,
         setUserId,
+        company,
+        setCompany,
+        phoneNumber,
+        setPhoneNumber,
+        billingAddress,
+        setBillingAddress,
       }}
     >
       {children}
     </AppContext.Provider>
   );
 };
-
 //! Custom hook for using context within app
 const useGlobalContext = () => {
   return useContext(AppContext);
 };
-
 export { AppProvider, useGlobalContext };
