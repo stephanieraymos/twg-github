3fb280c7801f1f844d165f65efcdfd402510254c
Stephanie Raymos <sraymos@rlliquidators.com>
2021-03-23 09:35:03 -0700

    TP-42 Modal updates with Iev

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect, useRef } from "react";
 import { FaTimes } from "react-icons/fa";
 import { Button, Modal, Form } from "react-bootstrap";
 import { useGlobalContext } from "../context";
-import { Link } from "react-router-dom";
 import Alert from "./Alert";
 import modalandsidebar from "../css/modalandsidebar.css";
 
@@ -16,15 +15,40 @@ const getLocalStorage = () => {
 };
 
 const LoginModal = () => {
+  const url = "http://143.110.225.28/v1/account/login/";
+
   const { isModalOpen, closeModal } = useGlobalContext();
 
-  const [person, setPerson] = useState(getLocalStorage());
-  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+  const {
+    userId,
+    setUserId,
+    email,
+    setEmail,
+    firstName,
+    setFirstName,
+    lastName,
+    setLastName,
+    company,
+    setCompany,
+    phoneNumber,
+    setPhoneNumber,
+    billingAddress,
+    setBillingAddress,
+  } = useGlobalContext();
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (!email || !password) {
+      showAlert(true, "danger", "Please enter value");
+    } else {
+      // Show alert and add person to person list only if name is true and not editing
+      showAlert(true, "success", "Person Added");
+
+      setEmail("");
+      setPassword("");
+    }
   };
 
   //showAlert function, when called the values for each param are passed in as arguments
@@ -32,10 +56,43 @@ const LoginModal = () => {
     setAlert({ show, type, msg });
   };
 
-  //useEffect happens only when person array changes. The person gets saved to localStorage
-  useEffect(() => {
-    localStorage.setItem("Person", JSON.stringify(person));
-  }, [person]);
+  //* useEffect for user post request
+  const login = () => {
+    console.log("login is running");
+    fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    })
+      .then((response) => {
+        console.log(response);
+        if (response.ok) {
+          return response.json();
+        } else if (response.status == 400) {
+          throw new Error("One or more of the required keys are missing.");
+        } else if (response.status == 404) {
+          throw new Error(
+            "The user does not exist or email/password is incorrect."
+          );
+        }
+      })
+      .then((user) => {
+        closeModal();
+        setUserId(user["id"]);
+        setEmail(user["email"]);
+        setFirstName(user["first_name"]);
+        setLastName(user["last_name"]);
+        setCompany(user["company"]);
+        setPhoneNumber(user["phone_number"]);
+        setBillingAddress(user["billing_address"]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <div
@@ -69,14 +126,14 @@ const LoginModal = () => {
                 required
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                placeholder="Email"
+                placeholder="Email Address"
               />
               <Form.Label style={{ color: "white" }}>Password</Form.Label>
               <Form.Control
-                type="text"
+                type="password"
                 required
                 value={password}
-                onChange={(e) => setEmail(e.target.value)}
+                onChange={(e) => setPassword(e.target.value)}
                 placeholder="Password"
               />
             </Form.Group>
@@ -89,7 +146,7 @@ const LoginModal = () => {
             </a>
             <Button
               type="submit"
-              onClick={closeModal}
+              onClick={login}
               className="boot-button"
               style={{ textAlign: "center" }}
             >
