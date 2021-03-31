1034ec0e75e4bbcc75b166306704810025d8f3a7
Stephanie Raymos <sraymos@rlliquidators.com>
2021-03-31 10:06:11 -0700

    Merge

--- src/auth.js
+++ src/auth.js
@@ -1,87 +1,90 @@
 import React, { useState, useEffect, useContext } from "react";
 import { useCookies } from "react-cookie";
 
-const AuthContext = React.createContext(null)
+const AuthContext = React.createContext(null);
 
 const AuthProvider = ({ children }) => {
-    // URLs to communicate with the auth API
-    const tokenVerifyURL = "https://api.thewholesalegroup.com/v1/account/token/verify/";
-    const tokenRefreshURL = "https://api.thewholesalegroup.com/v1/account/token/refresh/";
+  // URLs to communicate with the auth API
+  const tokenVerifyURL =
+    "https://api.thewholesalegroup.com/v1/account/token/verify/";
+  const tokenRefreshURL =
+    "https://api.thewholesalegroup.com/v1/account/token/refresh/";
 
-    // keys for the cookies
-    const accessTokenKey = "user-access-token";
-    const refreshTokenKey = "user-refresh-token";
+  // keys for the cookies
+  const accessTokenKey = "user-access-token";
+  const refreshTokenKey = "user-refresh-token";
 
-    const [cookies, setCookie, removeCookie] = useCookies([accessTokenKey, refreshTokenKey]);
-    
-    const removeToken = () => {
-        removeCookie(accessTokenKey);
-        removeCookie(refreshTokenKey);
-    }
+  const [cookies, setCookie, removeCookie] = useCookies([
+    accessTokenKey,
+    refreshTokenKey,
+  ]);
 
-    const setAccessToken = (token) => {
-        setCookie(accessTokenKey, token, {
-          path: "/",
-          // secure: true,
-          maxAge: 3600, // 1 hour
-        });
-    };
+  const removeToken = () => {
+    removeCookie(accessTokenKey);
+    removeCookie(refreshTokenKey);
+  };
 
-    const accessToken = () => cookies[accessTokenKey];
-    
-    const setRefreshToken = (token) => {
-        setCookie(refreshTokenKey, token, {
-            path: "/",
-            // secure: true,
-            maxAge: 604800, // 7 days
-        });
-    };
+  const setAccessToken = (token) => {
+    setCookie(accessTokenKey, token, {
+      path: "/",
+      // secure: true,
+      maxAge: 3600, // 1 hour
+    });
+  };
 
-    const refreshToken = () => cookies[refreshTokenKey];
+  const accessToken = () => cookies[accessTokenKey];
 
-    const authenticate = (success=() => {}, failure=() => {}) => {
-        // check whether there's cookies to check
-        if (cookies["user-access-token"]) {
+  const setRefreshToken = (token) => {
+    setCookie(refreshTokenKey, token, {
+      path: "/",
+      // secure: true,
+      maxAge: 604800, // 7 days
+    });
+  };
+
+  const refreshToken = () => cookies[refreshTokenKey];
+
+  const authenticate = (success = () => {}, failure = () => {}) => {
+    // check whether there's cookies to check
+    if (cookies["user-access-token"]) {
+      success();
+      return;
+    } else if (cookies["user-refresh-token"]) {
+      // user might have a refresh token that have not expired yet
+      fetch(tokenRefreshURL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          refresh: cookies["user-refresh-token"],
+        }),
+      })
+        .then((response) => {
+          if (response.ok) {
+            // user is logged in again
+            const user = response.json();
+            setAccessToken(user["access"]);
             success();
             return;
-        } else if (cookies["user-refresh-token"]) {
-            // user might have a refresh token that have not expired yet
-            fetch(tokenRefreshURL, {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({
-                    refresh: cookies["user-refresh-token"],
-                }),
-            })
-            .then((response) => {
-                if (response.ok) {
-                    // user is logged in again
-                    const user = response.json();
-                    setAccessToken(user["access"]);
-                    success();
-                    return;
-                }
-            })
-            .catch((error) => {
-                
-            });
-        }
-
-        failure();
-    };
-
-    const data = {
-        accessToken: [accessToken, setAccessToken],
-        refreshToken: [refreshToken, setRefreshToken],
-        authenticate,
-        removeToken
+          }
+        })
+        .catch((error) => {});
     }
 
-    return <AuthContext.Provider value={data}>{children}</AuthContext.Provider>
-}
+    failure();
+  };
+
+  const data = {
+    accessToken: [accessToken, setAccessToken],
+    refreshToken: [refreshToken, setRefreshToken],
+    authenticate,
+    removeToken,
+  };
+
+  return <AuthContext.Provider value={data}>{children}</AuthContext.Provider>;
+};
 
 const useAuthContext = () => {
-    return useContext(AuthContext);
+  return useContext(AuthContext);
 };
 
 export { AuthProvider, useAuthContext };
