49f113cd755fc40beb5d6972f45d40d46295bd57
Stephanie Raymos <sraymos@rlliquidators.com>
2021-04-07 15:54:36 -0700

    Moved Truck Details Card into it's own component

--- src/Components/Inventory/TruckDetails.js
+++ src/Components/Inventory/TruckDetails.js
@@ -1,18 +1,12 @@
 import React, { useState, useEffect } from "react";
-import {
-  FaAngleDoubleLeft,
-  FaTimes,
-  FaEdit,
-  FaAngleDoubleDown,
-} from "react-icons/fa";
+import { FaAngleDoubleLeft } from "react-icons/fa";
 import Navigation from "../Navigation/Navigation";
 // import { useTruck } from "../truckContext";
 import { useParams, Link, useHistory } from "react-router-dom";
 import Loading from "../../Pages/Loading";
-import logo from "../../img/w-logo.png";
-import { Card, Accordion } from "react-bootstrap";
 import { useAuthContext } from "../../auth";
 import { getByIdURL, inventoryURL, manifestURL } from "../../Pages/urls";
+import TruckDetailsCard from "./TruckDetailsCard";
 
 const TruckDetails = () => {
   const { id } = useParams();
@@ -156,280 +150,22 @@ const TruckDetails = () => {
         </Link>
       </div>
 
-      <section className="truck-section">
-        <h2 className="truck-details-header">{source}</h2>
-        <div className="truck">
-          <img src={logo} alt={source} style={{ size: "10rem" }} />
-          <div className="truck-info">
-            {/* //^ TRUCK ID CARD */}
-            <Card style={{ border: "none" }}>
-              <Card.Header
-                style={{
-                  padding: 0,
-                  borderBottom: "none",
-                  borderRadius: ".4rem",
-                }}
-              >
-                <p className="data-wrapper">
-                  <span className="truck-data-title">ID: </span>
-                  <span className="truck-data" style={{ paddingTop: ".5rem" }}>
-                    {loadId}
-                  </span>
-                </p>
-              </Card.Header>
-            </Card>
-            {/* //^ TRUCK NAME CARD */}
-            <Card style={{ border: "none" }}>
-              <Card.Header
-                style={{
-                  padding: 0,
-                  borderBottom: "none",
-                  borderRadius: ".4rem",
-                }}
-              >
-                <p className="data-wrapper">
-                  <span className="truck-data-title">Program: </span>
-                  <span className="truck-data" style={{ paddingTop: ".5rem" }}>
-                    {source}
-                  </span>
-                </p>
-              </Card.Header>
-            </Card>
-            {/* //^ TRUCK PRICE CARD */}
-            <Card style={{ border: "none" }}>
-              <Card.Header
-                style={{
-                  padding: 0,
-                  borderBottom: "none",
-                  borderRadius: ".4rem",
-                }}
-              >
-                <p className="data-wrapper">
-                  <span className="truck-data-title">Price: </span>
-                  <span className="truck-data" style={{ paddingTop: ".5rem" }}>
-                    {price}
-                  </span>
-                </p>
-              </Card.Header>
-            </Card>
-            {/* //^ TRUCK RETAIL PRICE CARD */}
-            <Card style={{ border: "none" }}>
-              <Card.Header
-                style={{
-                  padding: 0,
-                  borderBottom: "none",
-                  borderRadius: ".4rem",
-                }}
-              >
-                <p className="data-wrapper">
-                  <span className="truck-data-title">Retail Price: </span>
-                  <span className="truck-data" style={{ paddingTop: ".5rem" }}>
-                    {retailPrice}
-                  </span>
-                </p>
-              </Card.Header>
-            </Card>
-            {/* //^ CATEGORY CARD */}
-            <Card style={{ border: "none" }}>
-              <Card.Header
-                style={{
-                  padding: 0,
-                  borderBottom: "none",
-                  borderRadius: ".4rem",
-                }}
-              >
-                <p className="data-wrapper">
-                  <span className="truck-data-title">Category: </span>
-                  <span className="truck-data" style={{ paddingTop: ".5rem" }}>
-                    {category}
-                  </span>
-                </p>
-              </Card.Header>
-            </Card>
-            {/* //^ UNITS CARD */}
-            <Card style={{ border: "none" }}>
-              <Card.Header
-                style={{
-                  padding: 0,
-                  borderBottom: "none",
-                  borderRadius: ".4rem",
-                }}
-              >
-                <p className="data-wrapper">
-                  <span className="truck-data-title">Units: </span>
-                  <span className="truck-data" style={{ paddingTop: ".5rem" }}>
-                    {units}
-                  </span>
-                </p>
-              </Card.Header>
-            </Card>
-            {/* //^ PALLETS CARD */}
-            <Card style={{ border: "none" }}>
-              <Card.Header
-                style={{
-                  padding: 0,
-                  borderBottom: "none",
-                  borderRadius: ".4rem",
-                }}
-              >
-                <p className="data-wrapper">
-                  <span className="truck-data-title">Pallets: </span>
-                  <span className="truck-data" style={{ paddingTop: ".5rem" }}>
-                    {palletCount}
-                  </span>
-                </p>
-              </Card.Header>
-            </Card>
-            {/* //^ FOB CARD */}
-            <Card style={{ border: "none" }}>
-              <Card.Header
-                style={{
-                  padding: 0,
-                  borderBottom: "none",
-                  borderRadius: ".4rem",
-                }}
-              >
-                <p className="data-wrapper">
-                  <span className="truck-data-title">FOB: </span>
-                  <span className="truck-data" style={{ paddingTop: ".5rem" }}>
-                    {fob}
-                  </span>
-                </p>
-              </Card.Header>
-            </Card>
-            {/* //^ STATUS CARD */}
-            <Card style={{ border: "none" }}>
-              <Card.Header
-                style={{
-                  padding: 0,
-                  borderBottom: "none",
-                  borderRadius: ".4rem",
-                }}
-              >
-                <p className="data-wrapper">
-                  <span className="truck-data-title">status: </span>
-                  <span className="truck-data" style={{ paddingTop: ".5rem" }}>
-                    {status ? "Available" : "Sold"}
-                  </span>
-                </p>
-              </Card.Header>
-            </Card>
-            {/* //^ TRUCK CONTENTS ACCORDION */}
-            <Accordion>
-              <Card style={{ border: "none" }}>
-                <Accordion.Toggle
-                  as={Card.Header}
-                  eventKey="0"
-                  style={{
-                    padding: 0,
-                    borderBottom: "none",
-                    borderRadius: ".4rem",
-                  }}
-                >
-                  <p className="data-wrapper">
-                    <span className="truck-data-title">Contents: </span>
-                    <span className="truck-data-title">
-                      <FaAngleDoubleDown />
-                    </span>
-                  </p>
-                </Accordion.Toggle>
-                <Accordion.Collapse eventKey="0">
-                  <Card.Body
-                    style={{ color: "black", backgroundColor: "transparent" }}
-                  >
-                    <span>{contents}</span>
-                  </Card.Body>
-                </Accordion.Collapse>
-              </Card>
-            </Accordion>
-            {/* //^ TRUCK FILES ACCORDION */}
-            <Accordion>
-              <Card style={{ border: "none" }}>
-                <Accordion.Toggle
-                  as={Card.Header}
-                  eventKey="0"
-                  style={{
-                    padding: 0,
-                    borderBottom: "none",
-                    borderRadius: ".4rem",
-                  }}
-                >
-                  <p className="data-wrapper">
-                    <span className="truck-data-title">Files: </span>
-                    <span className="truck-data-title">
-                      <FaAngleDoubleDown />
-                    </span>
-                  </p>
-                </Accordion.Toggle>
-                <Accordion.Collapse eventKey="0">
-                  <Card.Body style={{ backgroundColor: "transparent" }}>
-                    {files.map((manifest, index) => {
-                      const { manifests, manifestName } = manifest;
-                      return (
-                        <ul key={manifestIds[index]}>
-                          <li
-                            onClick={
-                              () =>
-                                window.open(manifests, "_blank") ||
-                                window.location.replace(manifests) //Opens in new tab || Opens in same tab if pop ups are blocked
-                            }
-                          >
-                            <span style={{ cursor: "pointer", color: "black" }}>
-                              {manifestName}
-                            </span>
-                          </li>
-                        </ul>
-                      );
-                    })}
-                  </Card.Body>
-                </Accordion.Collapse>
-              </Card>
-            </Accordion>
-            {/* //^ TRUCK MODIFY ACCORDION */}
-            <Accordion>
-              <Card style={{ border: "none" }}>
-                <Accordion.Toggle
-                  as={Card.Header}
-                  eventKey="0"
-                  style={{
-                    padding: 0,
-                    borderBottom: "none",
-                    borderRadius: ".4rem",
-                  }}
-                >
-                  <p className="data-wrapper">
-                    <span className="truck-data-title">Modify </span>
-                    <span className="truck-data-title">
-                      <FaAngleDoubleDown />
-                    </span>
-                  </p>
-                </Accordion.Toggle>
-                <Accordion.Collapse eventKey="0">
-                  <Card.Body style={{ backgroundColor: "transparent" }}>
-                    <p className="data-wrapper">
-                      <button
-                        onClick={(e) => {
-                          e.preventDefault();
-                          deleteTruck();
-                        }}
-                        className="delete-truck-btn"
-                      >
-                        <FaTimes /> Delete this truck
-                      </button>
-                      <Link
-                        className="edit-truck-btn"
-                        to={`/UpdateTruckDetails/${id}`}
-                      >
-                        <FaEdit /> Edit this truck
-                      </Link>
-                    </p>
-                  </Card.Body>
-                </Accordion.Collapse>
-              </Card>
-            </Accordion>
-          </div>
-        </div>
-      </section>
+      <TruckDetailsCard
+        source={source}
+        loadId={loadId}
+        price={price}
+        retailPrice={retailPrice}
+        category={category}
+        units={units}
+        palletCount={palletCount}
+        fob={fob}
+        contents={contents}
+        files={files}
+        manifestIds={manifestIds}
+        deleteTruck={deleteTruck}
+        id={id}
+        status={status}
+      />
     </>
   );
 };
---
+++ src/Components/Inventory/TruckDetailsCard.js
@@ -0,0 +1,308 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../img/w-logo.png";
+import { Card, Accordion } from "react-bootstrap";
+import {
+  FaAngleDoubleLeft,
+  FaTimes,
+  FaEdit,
+  FaAngleDoubleDown,
+} from "react-icons/fa";
+
+const TruckDetailsCard = ({
+  source,
+  loadId,
+  price,
+  retailPrice,
+  category,
+  units,
+  palletCount,
+  fob,
+  contents,
+  files,
+  manifestIds,
+  id,
+  deleteTruck,
+  status,
+}) => {
+  return (
+    <>
+      <section className="truck-section">
+        <h2 className="truck-details-header">{source}</h2>
+        <div className="truck">
+          <img src={logo} alt={source} style={{ size: "10rem" }} />
+          <div className="truck-info">
+            {/* //^ TRUCK ID CARD */}
+            <Card style={{ border: "none" }}>
+              <Card.Header
+                style={{
+                  padding: 0,
+                  borderBottom: "none",
+                  borderRadius: ".4rem",
+                }}
+              >
+                <p className="data-wrapper">
+                  <span className="truck-data-title">ID: </span>
+                  <span className="truck-data" style={{ paddingTop: ".5rem" }}>
+                    {loadId}
+                  </span>
+                </p>
+              </Card.Header>
+            </Card>
+            {/* //^ TRUCK NAME CARD */}
+            <Card style={{ border: "none" }}>
+              <Card.Header
+                style={{
+                  padding: 0,
+                  borderBottom: "none",
+                  borderRadius: ".4rem",
+                }}
+              >
+                <p className="data-wrapper">
+                  <span className="truck-data-title">Program: </span>
+                  <span className="truck-data" style={{ paddingTop: ".5rem" }}>
+                    {source}
+                  </span>
+                </p>
+              </Card.Header>
+            </Card>
+            {/* //^ TRUCK PRICE CARD */}
+            <Card style={{ border: "none" }}>
+              <Card.Header
+                style={{
+                  padding: 0,
+                  borderBottom: "none",
+                  borderRadius: ".4rem",
+                }}
+              >
+                <p className="data-wrapper">
+                  <span className="truck-data-title">Price: </span>
+                  <span className="truck-data" style={{ paddingTop: ".5rem" }}>
+                    {price}
+                  </span>
+                </p>
+              </Card.Header>
+            </Card>
+            {/* //^ TRUCK RETAIL PRICE CARD */}
+            <Card style={{ border: "none" }}>
+              <Card.Header
+                style={{
+                  padding: 0,
+                  borderBottom: "none",
+                  borderRadius: ".4rem",
+                }}
+              >
+                <p className="data-wrapper">
+                  <span className="truck-data-title">Retail Price: </span>
+                  <span className="truck-data" style={{ paddingTop: ".5rem" }}>
+                    {retailPrice}
+                  </span>
+                </p>
+              </Card.Header>
+            </Card>
+            {/* //^ CATEGORY CARD */}
+            <Card style={{ border: "none" }}>
+              <Card.Header
+                style={{
+                  padding: 0,
+                  borderBottom: "none",
+                  borderRadius: ".4rem",
+                }}
+              >
+                <p className="data-wrapper">
+                  <span className="truck-data-title">Category: </span>
+                  <span className="truck-data" style={{ paddingTop: ".5rem" }}>
+                    {category}
+                  </span>
+                </p>
+              </Card.Header>
+            </Card>
+            {/* //^ UNITS CARD */}
+            <Card style={{ border: "none" }}>
+              <Card.Header
+                style={{
+                  padding: 0,
+                  borderBottom: "none",
+                  borderRadius: ".4rem",
+                }}
+              >
+                <p className="data-wrapper">
+                  <span className="truck-data-title">Units: </span>
+                  <span className="truck-data" style={{ paddingTop: ".5rem" }}>
+                    {units}
+                  </span>
+                </p>
+              </Card.Header>
+            </Card>
+            {/* //^ PALLETS CARD */}
+            <Card style={{ border: "none" }}>
+              <Card.Header
+                style={{
+                  padding: 0,
+                  borderBottom: "none",
+                  borderRadius: ".4rem",
+                }}
+              >
+                <p className="data-wrapper">
+                  <span className="truck-data-title">Pallets: </span>
+                  <span className="truck-data" style={{ paddingTop: ".5rem" }}>
+                    {palletCount}
+                  </span>
+                </p>
+              </Card.Header>
+            </Card>
+            {/* //^ FOB CARD */}
+            <Card style={{ border: "none" }}>
+              <Card.Header
+                style={{
+                  padding: 0,
+                  borderBottom: "none",
+                  borderRadius: ".4rem",
+                }}
+              >
+                <p className="data-wrapper">
+                  <span className="truck-data-title">FOB: </span>
+                  <span className="truck-data" style={{ paddingTop: ".5rem" }}>
+                    {fob}
+                  </span>
+                </p>
+              </Card.Header>
+            </Card>
+            {/* //^ STATUS CARD */}
+            <Card style={{ border: "none" }}>
+              <Card.Header
+                style={{
+                  padding: 0,
+                  borderBottom: "none",
+                  borderRadius: ".4rem",
+                }}
+              >
+                <p className="data-wrapper">
+                  <span className="truck-data-title">status: </span>
+                  <span className="truck-data" style={{ paddingTop: ".5rem" }}>
+                    {status ? "Available" : "Sold"}
+                  </span>
+                </p>
+              </Card.Header>
+            </Card>
+            {/* //^ TRUCK CONTENTS ACCORDION */}
+            <Accordion>
+              <Card style={{ border: "none" }}>
+                <Accordion.Toggle
+                  as={Card.Header}
+                  eventKey="0"
+                  style={{
+                    padding: 0,
+                    borderBottom: "none",
+                    borderRadius: ".4rem",
+                  }}
+                >
+                  <p className="data-wrapper">
+                    <span className="truck-data-title">Contents: </span>
+                    <span className="truck-data-title">
+                      <FaAngleDoubleDown />
+                    </span>
+                  </p>
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="0">
+                  <Card.Body
+                    style={{ color: "black", backgroundColor: "transparent" }}
+                  >
+                    <span>{contents}</span>
+                  </Card.Body>
+                </Accordion.Collapse>
+              </Card>
+            </Accordion>
+            {/* //^ TRUCK FILES ACCORDION */}
+            <Accordion>
+              <Card style={{ border: "none" }}>
+                <Accordion.Toggle
+                  as={Card.Header}
+                  eventKey="0"
+                  style={{
+                    padding: 0,
+                    borderBottom: "none",
+                    borderRadius: ".4rem",
+                  }}
+                >
+                  <p className="data-wrapper">
+                    <span className="truck-data-title">Files: </span>
+                    <span className="truck-data-title">
+                      <FaAngleDoubleDown />
+                    </span>
+                  </p>
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="0">
+                  <Card.Body style={{ backgroundColor: "transparent" }}>
+                    {files.map((manifest, index) => {
+                      const { manifests, manifestName } = manifest;
+                      return (
+                        <ul key={manifestIds[index]}>
+                          <li
+                            onClick={
+                              () =>
+                                window.open(manifests, "_blank") ||
+                                window.location.replace(manifests) //Opens in new tab || Opens in same tab if pop ups are blocked
+                            }
+                          >
+                            <span style={{ cursor: "pointer", color: "black" }}>
+                              {manifestName}
+                            </span>
+                          </li>
+                        </ul>
+                      );
+                    })}
+                  </Card.Body>
+                </Accordion.Collapse>
+              </Card>
+            </Accordion>
+            {/* //^ TRUCK MODIFY ACCORDION */}
+            <Accordion>
+              <Card style={{ border: "none" }}>
+                <Accordion.Toggle
+                  as={Card.Header}
+                  eventKey="0"
+                  style={{
+                    padding: 0,
+                    borderBottom: "none",
+                    borderRadius: ".4rem",
+                  }}
+                >
+                  <p className="data-wrapper">
+                    <span className="truck-data-title">Modify </span>
+                    <span className="truck-data-title">
+                      <FaAngleDoubleDown />
+                    </span>
+                  </p>
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="0">
+                  <Card.Body style={{ backgroundColor: "transparent" }}>
+                    <p className="data-wrapper">
+                      <button
+                        onClick={(e) => {
+                          e.preventDefault();
+                          deleteTruck();
+                        }}
+                        className="delete-truck-btn"
+                      >
+                        <FaTimes /> Delete this truck
+                      </button>
+                      <Link
+                        className="edit-truck-btn"
+                        to={`/UpdateTruckDetails/${id}`}
+                      >
+                        <FaEdit /> Edit this truck
+                      </Link>
+                    </p>
+                  </Card.Body>
+                </Accordion.Collapse>
+              </Card>
+            </Accordion>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default TruckDetailsCard;
