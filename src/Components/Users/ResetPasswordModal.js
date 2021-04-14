d9295bf9c8d832f477fd655ab8fb4ab16fd66002
Stephanie Raymos <sraymos@rlliquidators.com>
2021-04-14 09:13:00 -0700

    Working on reset password

--- src/Components/Users/ResetPasswordModal.js
+++ src/Components/Users/ResetPasswordModal.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Form, Modal, Button } from "react-bootstrap";
 import cancel from "../../img/cancel.svg";
+import { useGlobalContext } from "../../context";
+import mail from "../../img/mail.svg";
 
 const ResetPasswordModal = ({
   validated,
@@ -13,6 +15,12 @@ const ResetPasswordModal = ({
 }) => {
   document.title = "Reset Password";
 
+  const {
+    isModalOpen,
+    isResetEmailSuccess,
+    setIsResetEmailSuccess,
+  } = useGlobalContext();
+
   const closeModal = () => {
     setIsResetModalOpen(false);
   };
@@ -20,80 +28,141 @@ const ResetPasswordModal = ({
 
   return (
     <>
-      <Modal show={isResetModalOpen} onHide={closeModal}>
-        <div
-          className="form-body-container"
-          style={{ width: "25rem", alignSelf: "center" }}
+      {isResetEmailSuccess ? (
+        <Modal
+          show={isModalOpen}
+          onHide={closeModal}
+          backdrop="static"
+          centered
         >
-          {/* //^ MODAL HEADER */}
           <div
-            className="form-header-container"
-            style={{
-              width: "90%",
-              flexDirection: "row",
-              justifyContent: "space-between",
-              margin: "1rem 1rem 0rem",
-            }}
+            className="form-body-container"
+            style={{ width: "90%", alignSelf: "center" }}
           >
             <div
-              style={{ color: "black", fontSize: "36px" }}
-            >
-              Reset Password
-            </div>
-            {/* //^ CLOSE MODAL BUTTON */}
-            <button
+              className="form-header-container"
               style={{
-                background: "transparent",
-                borderColor: "transparent",
+                width: "85%",
+                flexDirection: "row",
+                justifyContent: "flex-end",
+                margin: "1rem 1rem 0rem",
               }}
             >
-              <img src={cancel} alt="cancel" onClick={closeModal} />
-            </button>
-          </div>
-          <hr
-            style={{
-              width: "100%",
-              height: "1px",
-              backgroundColor: "gray",
-              opacity: "25%",
-            }}
-          />
-          {/* //^ MODAL BODY */}
+              <button
+                style={{
+                  background: "transparent",
+                  borderColor: "transparent",
+                }}
+              >
+                <img
+                  src={cancel}
+                  alt="cancel"
+                  onClick={() => {
+                    setIsResetEmailSuccess(false);
+                    closeModal();
+                  }}
+                />
+              </button>
+            </div>
 
-          <Form
-            ref={form}
-            noValidate
-            validated={validated}
-            onSubmit={handleSubmit}
+            <hr
+              style={{
+                width: "100%",
+                height: "1px",
+                backgroundColor: "gray",
+                opacity: "25%",
+              }}
+            />
+
+            <img src={mail} alt="logo image" style={{ width: "100px" }} />
+            <div
+              className="form-label"
+              style={{
+                color: "black",
+                fontSize: "24px",
+                textAlign: "center",
+                margin: "0% 5% 5%",
+              }}
+            >
+              Please check your email for a link to reset your password.
+            </div>
+          </div>
+        </Modal>
+      ) : (
+        <Modal show={isResetModalOpen} onHide={closeModal}>
+          <div
+            className="form-body-container"
+            style={{ width: "25rem", alignSelf: "center" }}
           >
-            <Form.Group className="center-form-group">
-              <Form.Label className="form-label">Enter your email</Form.Label>
-              <Form.Control
-                type="text"
-                required
-                defaultValue={currentPassword}
-                name="source"
-                onChange={(e) => resetPassword(e.target.value)}
-              />
-              <Form.Control.Feedback type="invalid">
-                Please make sure to enter the email you used to create this account.
-              </Form.Control.Feedback>
-            </Form.Group>
-          </Form>
-          <Button
-            type="button"
-            className="form-button"
-            block
-            style={{
-              width: "200px",
-              backgroundColor: "#1f85b4",
-              marginBottom: "0.5rem",
-            }}
-          >
-            Send reset email
-          </Button>
-        </div>
-      </Modal>
+            {/* //^ MODAL HEADER */}
+            <div
+              className="form-header-container"
+              style={{
+                width: "90%",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                margin: "1rem 1rem 0rem",
+              }}
+            >
+              <div style={{ color: "black", fontSize: "36px" }}>
+                Reset Password
+              </div>
+              {/* //^ CLOSE MODAL BUTTON */}
+              <button
+                style={{
+                  background: "transparent",
+                  borderColor: "transparent",
+                }}
+              >
+                <img src={cancel} alt="cancel" onClick={closeModal} />
+              </button>
+            </div>
+            <hr
+              style={{
+                width: "100%",
+                height: "1px",
+                backgroundColor: "gray",
+                opacity: "25%",
+              }}
+            />
+            {/* //^ MODAL BODY */}
+
+            <Form
+              ref={form}
+              noValidate
+              validated={validated}
+              onSubmit={handleSubmit}
+            >
+              <Form.Group className="center-form-group">
+                <Form.Label className="form-label">Enter your email</Form.Label>
+                <Form.Control
+                  type="text"
+                  required
+                  defaultValue={currentPassword}
+                  name="source"
+                  onChange={(e) => resetPassword(e.target.value)}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please make sure to enter the email you used to create this
+                  account.
+                </Form.Control.Feedback>
+              </Form.Group>
+            </Form>
+            <Button
+              type="button"
+              className="form-button"
+              block
+              style={{
+                width: "200px",
+                backgroundColor: "#1f85b4",
+                marginBottom: "0.5rem",
+              }}
+            >
+              Send reset email
+            </Button>
+          </div>
+        </Modal>
+      )}
     </>
   );
 };
