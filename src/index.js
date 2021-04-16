e001473ff4f7cae4514cd45adf161fac94ddcb7f
Stephanie Raymos <sraymos@rlliquidators.com>
2021-04-16 10:54:06 -0700

    Registered service worker

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { CookiesProvider } from "react-cookie";
 import { AuthProvider } from "./auth";
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -24,4 +25,5 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
+reportWebVitals();
