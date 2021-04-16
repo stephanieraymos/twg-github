260c24fed253ceb8b50674ccf734eb0178f6864b
Stephanie Raymos <sraymos@rlliquidators.com>
2021-04-16 10:01:48 -0700

    Enabled service worker for navigator functionality

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
 import "./css/styles.css";
 import { AppProvider } from "./context";
 import App from "./App";
-import { TruckProvider } from "./truckContext";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { CookiesProvider } from "react-cookie";
-import { AuthProvider } from "./auth"
+import { AuthProvider } from "./auth";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -18,5 +18,10 @@ ReactDOM.render(
       </AuthProvider>
     </CookiesProvider>
   </React.StrictMode>,
+
   document.getElementById("root")
 );
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
