1f5ef32bfc8ce54349d8b9dfe27d0fde11043373
Stephanie Raymos <sraymos@rlliquidators.com>
2021-04-19 13:57:38 -0700

    Setting up development environment variables

--- src/Pages/urls.js
+++ src/Pages/urls.js
@@ -4,9 +4,6 @@ export const getByIdURL = "https://api.thewholesalegroup.com/v1/inventory/?id=";
 export const getByUserIdURL = "https://api.thewholesalegroup.com/v1/inventory/?userId=";
 export const getByLoadIdURL = "https://api.thewholesalegroup.com/v1/inventory/?loadId=";
 
-export const inventoryURL = "https://api.thewholesalegroup.com/v1/inventory/edit/";
-export const manifestURL = "https://api.thewholesalegroup.com/v1/inventory/manifest/";
-
 export const tokenVerifyURL = "https://api.thewholesalegroup.com/v1/account/token/verify/";
 export const tokenRefreshURL = "https://api.thewholesalegroup.com/v1/account/token/refresh/";
 export const emailVerificationURL = "https://api.thewholesalegroup.com/v1/account/register/verify/";
@@ -23,3 +20,15 @@ export const resetPasswordEmailURL = "https://api.thewholesalegroup.com/v1/accou
 export const resetPasswordURL = "https://api.thewholesalegroup.com/v1/account/password/reset/";
 
 export const superuserURL = "https://api.thewholesalegroup.com/v1/superuser/users/"
+
+export const development = true 
+
+if (development) {
+    const inventoryURL = "https://api.thewholesalegroup.com/test/v1/inventory/edit/";
+    const manifestURL = "https://api.thewholesalegroup.com/test/v1/inventory/manifest/";
+} else {
+    const inventoryURL = "https://api.thewholesalegroup.com/v1/inventory/edit/";
+    const manifestURL = "https://api.thewholesalegroup.com/v1/inventory/manifest/";
+}
+
+export const {inventoryURL, manifestURL}
