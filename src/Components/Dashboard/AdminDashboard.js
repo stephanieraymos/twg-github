bca4a7260c702cccea4d7c2799d6ec996e6352f1
Stephanie Raymos <sraymos@rlliquidators.com>
2021-04-22 16:04:22 -0700

    Setting up structure

--- src/Components/Dashboard/AdminDashboard.js
+++ src/Components/Dashboard/AdminDashboard.js
@@ -7,19 +7,27 @@ import OrderDetails from "../Orders/OrderDetails";
 
 const AdminDashboard = () => {
   document.title = "Dashboard";
+
+  //& -------- LOCAL STATE --------
   const [soldDay, setSoldDay] = useState(0);
+  const [availableInventory, setAvailableInventory] = useState([]);
+
+  //& -------- CONTEXT --------
   const [trucks] = useTruck();
+  const { inventory } = useInventoryContext();
 
-  const handleViewDetails = () => {
-    return <OrderDetails />;
-  };
+  //& -------- VARIABLES --------
 
-  //GROSS MARGIN
+  //& -------- FUNCTIONS --------
+
+  //^ GROSS MARGIN
   const grossMarginCalc = trucks.map(function (truck) {
     const { price, cost } = truck;
     return price - cost;
   });
+  const grossMargin = getArraySum(grossMarginCalc);
 
+  //^ GET SUM OF ARRAY
   function getArraySum(a) {
     var total = 0;
     for (var i in a) {
@@ -27,37 +35,46 @@ const AdminDashboard = () => {
     }
     return total;
   }
-  const grossMargin = getArraySum(grossMarginCalc);
 
-  const { inventory } = useInventoryContext();
-  const [availableInventory, setAvailableInventory] = useState([]);
+  //^ HANDLE ORDER DETAILS VIEW
+  const handleViewDetails = () => {
+    return <OrderDetails />;
+  };
 
+  //^ AVAILABLE INVENTORY
   useEffect(() => {
     setAvailableInventory(inventory.filter((item) => item.status === 2));
   }, [inventory]);
 
+  // @TODO Create a seperate component with reusable pieces for the dashboard
+
+  //& -------- RETURN --------
   return (
     <>
       <article className="admin-dashboard-content">
+        {/* //* ---- AVAILABLE INVENTORY ---- */}
         <div className="section-container">
           <div className="trucks-available section-items">
             <p className="section-items-desc">Available</p>
             <p className="dash-count">{availableInventory.length}</p>
           </div>
+          {/* //* ---- SOLD 24 HOURS ---- */}
           <div className="trucks-needing-approval section-items">
             <p className="section-items-desc">Sold 24 hrs</p>
             <p className="dash-count">{soldDay}</p>
           </div>
+          {/* //* ---- GROSS MARGIN ---- */}
           <div className="trucks-sold-30days section-items">
             <p className="section-items-desc">Gross Margin 24 hrs</p>
             <p className="dash-count">{grossMargin}</p>
           </div>
+          {/* //* ---- CONTACTS ---- */}
           <div className="trucks-contacts section-items">
             <p className="section-items-desc">Contacts</p>
             <p className="dash-count">64</p>
           </div>
         </div>
-
+        {/* //* ---- GRAPH ---- */}
         <div className="sales-graph-container">
           <div className="sales-graph">
             <p className="sales-graph-heading">Sales</p>
@@ -66,8 +83,9 @@ const AdminDashboard = () => {
           </div>
         </div>
         {/* <D3 /> */}
-
+        {/* //* ---- FOOTER CONTENT ---- */}
         <div className="bottom-content">
+          {/* //* ---- NEW LEADS ---- */}
           <div className="new-leads-container bottom-content-items">
             <div className="top-leads-container">
               <p className="leads-container-header">New Leads</p>
@@ -94,6 +112,7 @@ const AdminDashboard = () => {
               </div>
             </div>
           </div>
+          {/* //* ---- ORDERS NEEDING APPROVAL ---- */}
           <div className="orders-to-approve-container bottom-content-items">
             <div className="top-leads-container">
               <p className="leads-container-header">Orders Needing Approval</p>
