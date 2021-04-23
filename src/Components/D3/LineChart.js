b624ea4a7e9a2b7c30fa0ad8143298c46bdca8ef
Stephanie Raymos <sraymos@rlliquidators.com>
2021-04-23 13:26:20 -0700

    Function to change label based on selection

--- src/Components/D3/LineChart.js
+++ src/Components/D3/LineChart.js
@@ -12,18 +12,35 @@ const margin = { top: 20, right: 30, bottom: 65, left: 90 };
 const xAxisLabelOffset = 50;
 const yAxisLabelOffset = 60;
 
+const attributes = [
+  { value: "sold", label: "Date Sold" },
+  { value: "cost", label: "Our Cost" },
+  { value: "price", label: "Our Price" },
+  { value: "retail_price", label: "Retail Price" },
+  { value: "program", label: "program" },
+];
+
+// FUNCTION TO GET LABEL
+const getLabel = (value) => {
+  for (let i = 0; i < attributes.length; i++) {
+    if (attributes[i].value === value) {
+      return attributes[i].label;
+    }
+  }
+};
+
 const LineChart = () => {
   const data = useData();
 
   const initialXAttribute = "sold";
   const [xAttribute, setXAttribute] = useState(initialXAttribute);
   const xValue = (d) => d[xAttribute];
-  const xAxisLabel = "Date";
+  const xAxisLabel = getLabel(xAttribute);
 
   const initialYAttribute = "cost";
   const [yAttribute, setYAttribute] = useState(initialYAttribute);
   const yValue = (d) => d[yAttribute];
-  const yAxisLabel = "Sales";
+  const yAxisLabel = getLabel(yAttribute);
 
   if (!data) {
     return <pre>Loading...</pre>;
@@ -31,14 +48,6 @@ const LineChart = () => {
 
   const innerHeight = height - margin.top - margin.bottom;
   const innerWidth = width - margin.left - margin.right;
-
-  const attributes = [
-    { value: "sold", label: "Date Sold" },
-    { value: "cost", label: "Our Cost" },
-    { value: "price", label: "Our Price" },
-    { value: "retail_price", label: "Retail Price" },
-    { value: "program", label: "program" },
-  ];
 
   const xAxisTickFormat = timeFormat("%a");
 
