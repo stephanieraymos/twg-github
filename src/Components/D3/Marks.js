2467e4ae2d555b76220364b40aed6e792d1b2035
Stephanie Raymos <sraymos@rlliquidators.com>
2021-04-28 09:15:03 -0700

    Graph changes; going to refactor

--- src/Components/D3/Marks.js
+++ src/Components/D3/Marks.js
@@ -1,18 +1,41 @@
+// export const Marks = ({
+//   data,
+//   xScale,
+//   yScale,
+//   xValue,
+//   yValue,
+//   tooltipFormat,
+//   binnedData,
+// }) =>
+//   binnedData.map((d) => (
+//     <rect
+//       className="mark"
+//       x={xScale(xValue(d))}
+//       y={yScale(yValue(d))}
+//     >
+//       <title>{tooltipFormat(xValue(d))}</title>
+//     </rect>
+//   ));
+
 export const Marks = ({
-  data,
+  binnedData,
   xScale,
   yScale,
-  xValue,
-  yValue,
   tooltipFormat,
-  binnedData,
+  innerHeight,
+  mindate,
+  maxdate,
 }) =>
   binnedData.map((d) => (
     <rect
+      // Rectangles must have width and height
       className="mark"
-      x={xScale(xValue(d))}
-      y={yScale(yValue(d))}
+      x={xScale(xScale(d))} //Start date
+      y={yScale(yScale(d))}
+      width={xScale(maxdate) - xScale(mindate)} //Distance between x0 and x1 after being passed through the xScale
+      height={innerHeight - yScale(d.y)}
     >
-      <title>{tooltipFormat(xValue(d))}</title>
+      {/* Tooltip is sum (d.y) */}
+      <title>{tooltipFormat(d.y)}</title>
     </rect>
   ));
