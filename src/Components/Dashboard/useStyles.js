685a4e6467afb0e9fcbb02ddd3581d8f68a7829c
Stephanie Raymos <sraymos@rlliquidators.com>
2021-04-28 10:39:46 -0700

    Huge admin dashboard refactor

---
+++ src/Components/Dashboard/useStyles.js
@@ -0,0 +1,36 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    padding: theme.spacing(5),
+    backgroundColor: "#f7f8fc",
+  },
+  card: {
+    borderRadius: 15,
+  },
+  title: {
+    fontFamily: "Montserrat, sans-serif",
+    fontWeight: 700,
+    color: theme.palette.text.secondary,
+  },
+  body: {
+    fontFamily: "Montserrat, sans-serif",
+    color: theme.palette.text.se,
+  },
+  title_centered: {
+    fontFamily: "Montserrat, sans-serif",
+    textAlign: "center",
+    fontWeight: 700,
+    color: theme.palette.text.secondary,
+  },
+  body_centered: {
+    fontFamily: "Montserrat, sans-serif",
+    textAlign: "center",
+    color: theme.palette.text.primary,
+  },
+  button: {
+    fontFamily: "Montserrat, sans-serif",
+    margin: "auto",
+  },
+}));
