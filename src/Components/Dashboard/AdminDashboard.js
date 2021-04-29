266dec52020d59560962ed7a8f5908418209ce14
Stephanie Raymos <sraymos@rlliquidators.com>
2021-04-29 13:12:24 -0700

    Cleanup

--- src/Components/Dashboard/AdminDashboard.js
+++ src/Components/Dashboard/AdminDashboard.js
@@ -59,39 +59,14 @@ export default function AdminDashboard() {
 
   // ^ INVENTORIES CREATED 30+ DAYS AGO
   let today = new Date();
-  var priorDate = new Date().setDate(today.getDate() - 2); //Today 30 days ago
+  var priorDate = new Date().setDate(today.getDate() - 30); //Today 30 days ago
 
-  const createdList = inventory.map((item) => new Date(item.created)); //Creating an array of each instance of created and putting it into an array, parsed to a Date.
-  //   console.log(createdList);
 
-  createdList.forEach((val) => {
-    //Grabbing each individual date from createdList and checking to see if it's been created for 30 days or more.
-    if (val < priorDate) {
-      console.log("This was created more than 30 days ago");
-    }
-  });
   const oldItems = inventory.filter(
     (item) => new Date(item.created) < priorDate
   ); //Filtered array of all inv. items created 30 days ago or more.
   //   console.log(oldItems);
 
-  // inventory.map((item) => {
-  //   const { created } = item;
-
-  //   console.log("created", created)
-
-  //   const createdDate = new Date(created);
-  //   console.log(typeof createdDate)
-  //   console.log("created date", createdDate);
-  //   console.log("today", today);
-  //   if (+createdDate < priorDate) {
-  //     console.log("This was created more than 30 days ago");
-  //   }
-  //   console.log("prior date", priorDate)
-  //   console.log("today parsed to number", +today)
-  // });
-
-  
   useEffect(() => {
     const items = [];
     Object.entries(users).map(([_, value]) => {
