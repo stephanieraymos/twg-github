31f5f594f08b1330d34fa70dae81d4f5b000a90f
Stephanie Raymos <sraymos@rlliquidators.com>
2021-04-30 12:32:17 -0700

    Working on flexbox container

--- src/Components/Inventory/NarrowSearch/NarrowYourSearch.js
+++ src/Components/Inventory/NarrowSearch/NarrowYourSearch.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Nav, Card } from "react-bootstrap";
-import NarrowSearchForm from "./NarrowSearchForm"
+import NarrowSearchForm from "./NarrowSearchForm";
 
 const NarrowYourSearch = () => {
   return (
     <>
-      <Card className="col-md-12 d-none d-md-block bg-light narrow-sidebar">Narrow your search
-      <Card.Body>
+      <Card className="col-md-12 d-none d-md-block bg-light narrow-sidebar">
+        <Card.Title style={{borderBottom:"1px black solid"}}>Narrow your search</Card.Title>
+        <Card.Body>
           <NarrowSearchForm />
-      </Card.Body>
+        </Card.Body>
       </Card>
     </>
   );
