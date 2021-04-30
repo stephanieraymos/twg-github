5a37db7b19ba8a19f9ebf95373449fc79e78fd0a
Stephanie Raymos <sraymos@rlliquidators.com>
2021-04-30 12:18:28 -0700

    Narrow search feature added (Does not work yet, just display)

---
+++ src/Components/Inventory/NarrowSearch/NarrowSearchForm.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Form, Row, Col } from "react-bootstrap";
+
+const NarrowSearchForm = () => {
+  return (
+    <>
+      <Form>
+        <Form.Group>
+          <Form.Label>Location</Form.Label>
+          <Form.Check type="checkbox" label="Sacramento" />
+          <Form.Check type="checkbox" label="Woodland" />
+          <Form.Check type="checkbox" label="Roseville" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Categories</Form.Label>
+          <Form.Check type="checkbox" label="Clothing" />
+          <Form.Check type="checkbox" label="Office" />
+          <Form.Check type="checkbox" label="Home" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Size</Form.Label>
+          <Form.Check type="checkbox" label="100-1000 units" />
+          <Form.Check type="checkbox" label="1000-2500 units" />
+          <Form.Check type="checkbox" label="2500-5000 units" />
+          <Form.Check type="checkbox" label="5000-10000 units" />
+          <Form.Check type="checkbox" label="10000+ units" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Price Range</Form.Label>
+          <Form.Check type="checkbox" label="$5,000 and under" />
+          <Form.Check type="checkbox" label="$5,000 - $10,000" />
+          <Form.Check type="checkbox" label="$10,000 and up" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Condition</Form.Label>
+          <Form.Check type="checkbox" label="Overstock" />
+          <Form.Check type="checkbox" label="Returns" />
+          <Form.Check type="checkbox" label="Salvage" />
+          <Form.Check type="checkbox" label="Shelf Pulls" />
+          <Form.Check type="checkbox" label="Warehouse Damage" />
+        </Form.Group>
+      </Form>
+    </>
+  );
+};
+
+export default NarrowSearchForm;
--- src/Components/Inventory/NarrowSearch/NarrowYourSearch.js
+++ src/Components/Inventory/NarrowSearch/NarrowYourSearch.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { Nav, Card } from "react-bootstrap";
+import NarrowSearchForm from "./NarrowSearchForm"
 
 const NarrowYourSearch = () => {
   return (
     <>
-      <Card className="col-md-12 d-none d-md-block bg-light narrow-sidebar">CARD</Card>
+      <Card className="col-md-12 d-none d-md-block bg-light narrow-sidebar">Narrow your search
+      <Card.Body>
+          <NarrowSearchForm />
+      </Card.Body>
+      </Card>
     </>
   );
 };
